c6acff9fc33b6e5b06bb325b6292b36bd2c0805a
nbroeder <nbroeder@uwo.ca>
2021-07-17 17:07:53 -0400

    Created a nested for loop that adds an event listener to each button that is part of the minesweeper game. The event listener is added to the buttons using DOM navigation.

--- Minesweeper.js
+++ Minesweeper.js
@@ -1,28 +1,45 @@
-function button(){
-    this.checked=false;
-    this.flaggged=false;
-    this.mine=false;
-    this.adjacentMines=0;
-}
 
-// function createGame(size){
-//     const btn=[];
-//     for(let i=0; i<size*size;i++){
-//         btn[i]=new button;
-//     }
+ function createGame(){
+     document.getElementById('label').innerText="game had begun";
+    function button(){
+        this.checked=false;
+        this.flaggged=false;
+        this.mine=false;
+        this.adjacentMines=0;
+    }
+    const btn=[];
+    for(let i=0; i<100;i++){
+        btn[i]=new button;
+    }
 
-//     let table=document.createElement('table');
-//     let row=document.createElement('tr')
-//     table.id="table";
-//     row.innerHTML='<button>button</button>'
-//     table.appendChild(row);
-//     table.appendChild(row);
-//     table.appendChild(row);
-//     table.appendChild(row);
-//     table.appendChild(row);
-// }
+    for (let i =0;i<5;i++){
+        let rand=Math.floor(Math.random()*10);
+        if(btn[rand].mine==false){
+            btn[rand].mine=true;
+        }else{
+            i--;
+        }
+    }
+    
+    //Will add an event listener to each button in the game. The event listener calls a function that will check to see if the button is a mine. 
+    for (let i=0;i<20;i+=2){
+       
+        for (let j=1;j<20;j+=2){
+            document.getElementById('table').childNodes[1].childNodes[i].childNodes[j].firstChild.addEventListener('click',checkForMine(((i/2)*10+((j-1)/2))));
+            
+        }
+    }
 
-function createGame(){
-   
-    document.getElementById(btn).onclick="clicked";
+ }
+
+
+
+function checkForMine(i){
+    console.log(i);
+    /*if(btn[i].mine==false){
+        document.getElementById('label').innerHTML='pressed';
+     }else{
+         document.getElementById('label').innerHTML='Mine';
+     }*/
+
 }
